991b61e31caabd6fd125e4eccdb13efb44a01cbb
Pedro Jaeger <guijaeger@gmail.com>
2022-09-12 22:43:10 -0300

    update
    Form estruturado, pegando valores do campo e dando o push no array.

--- pages/components/forms/index.tsx
+++ pages/components/forms/index.tsx
@@ -5,62 +5,101 @@ import Container from "@mui/material/Container";
 import * as React from "react";
 import TextField from "@mui/material/TextField";
 import Autocomplete from "@mui/material/Autocomplete";
+import { useState } from "react";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
 
 const FormsApp = () => {
-  const formasDePagamento = [
-    { label: "PIX Marcus", pagamento: 1 },
-    { label: "PIX Angela", pagamento: 2 },
-    { label: "Cielo", pagamento: 3 },
-    { label: "Rede", pagamento: 4 },
-  ];
+  const vendas: any[] = [];
 
-  function lancarVenda() {
-    console.log("pagamento");
-  }
+  const [FormaDePagamento, setFormaDePagamento] = useState("");
+
+  const onChangeFormaDePagamento = (event: SelectChangeEvent) => {
+    setFormaDePagamento(event.target.value);
+  };
+
+  const [cliente, setCliente] = useState("");
+  const [valor, setValor] = useState("");
+
+  const lancarVenda = () => {
+    console.log("pagamento lançado");
+    console.log(cliente);
+    console.log(valor);
+    console.log(FormaDePagamento);
+    let venda = { cliente, valor, FormaDePagamento };
+    vendas.push(venda);
+    console.log(vendas);
+  };
+
+  const onChangeNome = (evt: SelectChangeEvent) => {
+    setCliente(evt.target.value as string);
+  };
+  const onChangeValor = (evt: any) => {
+    setValor(evt.target.value);
+  };
+  const onChangePagamento = (evt: any) => {
+    setValor(evt.target.value);
+  };
 
   return (
     <>
-      <React.Fragment>
-        <Container maxWidth="sm">
-          <Box
-            display="flex"
-            flexDirection="column"
-            alignItems="center"
-            marginTop="15px"
-            sx={{ bgcolor: "#cfe8fc", height: "80vh" }}
+      <Container maxWidth="sm" className="container">
+        <Box
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+          marginTop="15px"
+          sx={{ bgcolor: "#cfe8fc", height: "80vh" }}
+        >
+          <h1>asdasd</h1>
+
+          <TextField
+            className="form"
+            id="outlined-basic"
+            label="Nome Cliente"
+            variant="outlined"
+            name="cliente"
+            value={cliente}
+            onChange={onChangeNome}
+          />
+
+          <TextField
+            className="form"
+            id="outlined-number"
+            label="Valor"
+            type="number"
+            name="valor"
+            onChange={onChangeValor}
+            InputLabelProps={{
+              shrink: true,
+            }}
+          />
+
+          <InputLabel>Forma de Pagamento</InputLabel>
+          <Select
+            sx={{ minWidth: 120 }}
+            value={FormaDePagamento}
+            label="Forma de Pagamento"
+            onChange={onChangeFormaDePagamento}
           >
-            <h1 className="teste">asdasd</h1>
-            <TextField
-              id="outlined-basic"
-              label="Nome Cliente"
-              variant="outlined"
-            />
+            <MenuItem value={1}>Pix Marcus</MenuItem>
+            <MenuItem value={2}>Pix Angela</MenuItem>
+            <MenuItem value={3}>Rede</MenuItem>
+            <MenuItem value={4}>Cielo</MenuItem>
+          </Select>
 
-            <TextField
-              id="outlined-number"
-              label="Number"
-              type="number"
-              InputLabelProps={{
-                shrink: true,
-              }}
-            />
-
-            <Autocomplete
-              disablePortal
-              id="combo-box-demo"
-              options={formasDePagamento}
-              sx={{ width: 300 }}
-              renderInput={(params) => (
-                <TextField {...params} label="Forma de Pagamento" />
-              )}
-            />
-
-            <Button variant="contained" color="success" onClick={lancarVenda}>
-              Success
-            </Button>
-          </Box>
-        </Container>
-      </React.Fragment>
+          <Button
+            className="form"
+            variant="contained"
+            color="success"
+            type="submit"
+            onClick={lancarVenda}
+          >
+            Success
+          </Button>
+        </Box>
+      </Container>
     </>
   );
 };
